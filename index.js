c0257a8cc684b0d8d5bc36c62e5770284ea07251
Alex Chuang <ball16357@gmail.com>
2021-06-24 23:34:43 +0800

    API with unsplash

---
+++ index.js
@@ -0,0 +1,14 @@
+fetch("https://api.unsplash.com/photos/?client_id=cl1f0ZPa6hgwoazEYPwh_otBP90I7Es5ozZ1VgVK8Vw")
+.then((res) => {
+  const data = res.json();
+  return data;
+})
+.then((data) => {
+  console.log(data);
+  for (let i = 0; i < data.length; i++) {
+    let container = document.querySelector("#data-here");
+    let img = document.createElement("img");
+    img.setAttribute("src", data[i].urls.small);
+    container.append(img);
+  }
+});
